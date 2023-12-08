fd6ffcc61ee4e8ba878fab5920b9ce91057aa1d8
MaybeHazza <bakerharveyj@gmail.com>
2023-12-08 23:36:10 +0400

    Movement Code Update
    Climbing ladder code not working properly, need to fix

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,40 +6,70 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] float runSpeed = 10f; 
+    [SerializeField] float runSpeed = 10f;
+    [SerializeField] float jumpVerticalSpeed = 5f;
+    [SerializeField] float jumpHorizontalSpeed = -5f;
+    [SerializeField] float climbingLadderSpeed = -5f;
 
 
     Vector2 moveInput;
     Rigidbody2D myRigidbody;
+    Animator myAnimator;
+    BoxCollider2D myBoxCollider;
 
     void Start()
     {
-        myRigidbody = GetComponent<Rigidbody2D>();  
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        myBoxCollider = GetComponent<BoxCollider2D>();
     }
 
     void Update()
     {
         Run();
         FlipSprite();
+        ClimbLadder();
+    }
+
+    void ClimbLadder()
+    {
+        if (!myBoxCollider.IsTouchingLayers(LayerMask.GetMask("Ladders")))
+        { return; }
+
+        Vector2 climbVelocity = new Vector2 (myRigidbody.velocity.x, moveInput.y * climbingLadderSpeed);
+        myRigidbody.velocity = climbVelocity; 
     }
 
     void OnMove(InputValue value) 
     {
         moveInput = value.Get<Vector2>();
+    }
 
+    void OnJump(InputValue value) 
+    {
+        if (!myBoxCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))) { return; }
+
+        if (value.isPressed) 
+        {
+            myRigidbody.velocity += new Vector2(jumpHorizontalSpeed, jumpVerticalSpeed);
+        }
     }
 
     void Run() 
     { 
         Vector2 playerVelocity = new Vector2 (moveInput.x * runSpeed, myRigidbody.velocity.y);
         myRigidbody.velocity = playerVelocity;
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("isRunning", playerHasHorizontalSpeed);
     }
+
 
     void FlipSprite()
     {
-        bool payerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
 
-        if (payerHasHorizontalSpeed)
+        if (playerHasHorizontalSpeed)
         {
             transform.localScale = new Vector2(Mathf.Sign(myRigidbody.velocity.x), 1f);
         }
